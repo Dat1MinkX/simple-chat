0ab0490eacb1d71d827b7afdd5b72b89720b85fb
Dat1MinkX <thomatjoh191208@gmail.com>
2026-01-08 14:21:00 +0100

    i changed everything

--- script.js
+++ script.js
@@ -11,4 +11,17 @@ async function lastInn() {
         ul.appendChild(li);
     }
 }
-document.getElementById("loadBtn").addEventListener("click", lastInn);
+
+setInterval(lastInn, 5)
+/*side nav code*/
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
